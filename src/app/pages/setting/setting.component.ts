a80f091534deeeef664bd8f7aba33d05b6969778
mohacelhosen <mohacel.hosen22@gmail.com>
2023-08-14 20:21:18 +0600

    Setting Component Updated

--- src/app/pages/setting/setting.component.ts
+++ src/app/pages/setting/setting.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 import { PopupService } from 'src/app/services/popup.service';
 
 @Component({
@@ -7,26 +8,73 @@ import { PopupService } from 'src/app/services/popup.service';
   templateUrl: './setting.component.html',
   styleUrls: ['./setting.component.css'],
 })
-export class SettingComponent {
+export class SettingComponent implements OnInit {
   settingForm!: FormGroup;
   isSubmitting = false;
 
   constructor(
     private _formBuilder: FormBuilder,
-    private toast: PopupService
+    private toast: PopupService,
+    private apiService: ApiService
   ) {}
 
   ngOnInit(): void {
     this.settingForm = this._formBuilder.group({
       firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      dob: ['', Validators.required],
-      gender: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      designation: ['', Validators.required],
+      lastName: [''],
+      userPhoto: ['', Validators.required],
+      dob: [''],
+      linkedin: [''],
+      github: [''],
+      facebook: [''],
+    });
+
+    // Retrieve user data from API and populate form
+    this.apiService.getSingleUser().subscribe(
+      (user) => {
+        this.populateFormWithUserData(user);
+      },
+      (error) => {
+        console.error('API Error:', error);
+      }
+    );
+  }
+
+  populateFormWithUserData(user: any): void {
+    // Convert dob array to LocalDate
+    const dobArray = user.dob;
+    const dob = new Date(dobArray[0], dobArray[1] - 1, dobArray[2]);
+
+    // Populate form controls with user data
+    this.settingForm.patchValue({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userPhoto: user.userPhoto,
+      dob: dob,
+      linkedin: user.linkedin,
+      github: user.github,
+      facebook: user.facebook,
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.settingForm.valid) {
+      this.isSubmitting = true;
+
+      const updatedUserData = this.settingForm.value;
+
+      this.apiService.updateUser(updatedUserData).subscribe(
+        (response) => {
+          this.isSubmitting = false;
+          // Show success message or handle as needed
+          this.toast.showSuccessTopCenter('User information updated successfully.');
+        },
+        (error) => {
+          this.isSubmitting = false;
+          // Handle error if needed
+          this.toast.showErrorBottonCenter('Error updating user information.');
+        }
+      );
+    }
+  }
 }
