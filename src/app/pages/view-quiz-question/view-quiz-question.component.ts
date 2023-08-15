8c6615163a646d1f35fe280bfe2508b797787b80
mohacelhosen <mohacel.hosen22@gmail.com>
2023-08-15 16:04:50 +0600

    Routing Completed

---
+++ src/app/pages/view-quiz-question/view-quiz-question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-quiz-question',
+  templateUrl: './view-quiz-question.component.html',
+  styleUrls: ['./view-quiz-question.component.css']
+})
+export class ViewQuizQuestionComponent {
+
+}
